a9d650178032abe31a438be31ec70287881b1fb8
yogs0ddhoth <blin7.webdev@gmail.com>
2022-09-13 00:06:28 -0400

    aungular tutorial 5 complete

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable, Subject } from 'rxjs';
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
   styleUrls: ['./hero-search.component.css']
 })
 export class HeroSearchComponent implements OnInit {
+  heroes$!: Observable<Hero[]>;
+  #searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  /** Push a search term into the observable stream. */
+  search(term: string): void {
+    this.#searchTerms.next(term);
+  }
 
   ngOnInit(): void {
+    this.heroes$ = this.#searchTerms.pipe(
+      debounceTime(300), // wait 300ms after each keystroke before considering the term
+      distinctUntilChanged(), // ignore new term if same as previous term
+      switchMap( // switch to new search observable each time the term changes
+        (term: string) => this.heroService.searchHeroes(term)
+      )
+    )
   }
 
 }
