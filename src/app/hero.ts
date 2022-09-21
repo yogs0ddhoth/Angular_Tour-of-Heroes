86a43d8c2a25d9954618c110809d4fd018695f0d
yogs0ddhoth <blin7.webdev@gmail.com>
2022-09-21 12:32:44 -0400

    Hero Class created and integrated

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,21 @@
-export interface Hero {
-  id: number;
-  name: string;
+export class Hero {
+  #alterEgo?: string;
+
+  constructor(
+    public id: number,
+    public name: string,
+    public power: string,
+    alterEgo?: string
+  ) {
+    if (alterEgo) {
+      this.#alterEgo = alterEgo;
+    }
+  }
+
+  get alterEgo() { 
+    return this.#alterEgo 
+      ? this.#alterEgo 
+      : 'UNKNOWN'; 
+  }
+  set alterEgo(alterEgo: string) { this.#alterEgo = alterEgo; }
 }
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -4,7 +4,7 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Hero } from './hero';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
@@ -17,9 +17,11 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 17, name: 'Dynama' },
       { id: 18, name: 'Dr. IQ' },
       { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 20, name: 'Tornado' },
     ];
-    return {heroes};
+    heroes.map((hero) => new Hero(hero.id, hero.name, 'lorem ipsum'));
+
+    return { heroes };
   }
 
   /**
@@ -27,11 +29,9 @@ export class InMemoryDataService implements InMemoryDbService {
    * @returns If the heroes array is empty, the method below returns the initial number (11). Else, if the heroes array is not empty, the method below returns the highest hero id + 1.
    */
   genId(heroes: Hero[]): number {
-    return heroes.length > 0 
-      ? Math.max(
-          ...heroes.map(hero => hero.id)
-        ) + 1 
+    return heroes.length > 0
+      ? Math.max(...heroes.map((hero) => hero.id)) + 1
       : 11;
   }
-  constructor() { }
+  constructor() {}
 }
